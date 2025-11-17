a6631c6a99a3338db4eaa23992db00d6186e7215
Ryan Houdek <Sonicadvance1@gmail.com>
2025-11-17 09:57:54 -0800

    rename

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	clang++ -static -fuse-ld=lld -g -O2 -o fex_shm_stats_read fex_shm_stats_read.cpp -std=c++20 `pkgconf --libs --static ncursesw`
+	clang++ -static -fuse-ld=lld -g -O2 -o what-the-FEX what-the-FEX.cpp -std=c++20 `pkgconf --libs --static ncursesw`
--- watch.sh
+++ watch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Watching for $1"
-while true ; do while ! [ $(FEXpidof "$1") ] ; do sleep 1; done ; ./fex_shm_stats_read `FEXpidof "$1"` ; done
+while true ; do while ! [ $(FEXpidof "$1") ] ; do sleep 1; done ; ./what-the-FEX `FEXpidof "$1"` ; done
